0638620cbf144d8dc1e14b2ca63edb28fc04a65e
Erik Garduno <erikgarduo@gmail.com>
2020-04-18 15:06:43 -0500

    Firs comit creation of the new repo

---
+++ src/main/java/ignity/geo/generic/GeoGenericApplication.java
@@ -0,0 +1,13 @@
+package ignity.geo.generic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoGenericApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoGenericApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ignity/geo/generic/GeoGenericApplicationTests.java
@@ -0,0 +1,13 @@
+package ignity.geo.generic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoGenericApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
